b16e44e87cd36c0cb7acb78f1ba1a67e2f5bc0ce
Chatresh-hash <chatresh2017@gmail.com>
2026-06-18 11:17:47 +0530

    new

--- Assigments/Banking-structure/transaction-service/src/main/java/com/bank/TransactionApplication.java
+++ Assigments/Banking-structure/transaction-service/src/main/java/com/bank/TransactionApplication.java
@@ -1,8 +1,14 @@
 package com.bank;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.UUID;
 
+@SpringBootApplication
+@RestController
+@RequestMapping("/transactions")
 public class TransactionApplication {
 
     public static void main(String[] args) {
